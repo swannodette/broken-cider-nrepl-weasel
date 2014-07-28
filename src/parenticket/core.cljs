9b97cfd5d9e2817564015c13fc148bc0b07edb1e
Moritz Ulrich <moritz@tarn-vedra.de>
2014-07-28 16:21:58 +0200

    Minimal example project showing broken behavior.

---
+++ src/parenticket/core.cljs
@@ -0,0 +1,30 @@
+(ns parenticket.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+
+            [weasel.repl :as ws-repl]
+
+            [cljs.core.async :as async])
+  (:require-macros [cljs.core.async.macros :refer [go-loop]]))
+
+(enable-console-print!)
+(ws-repl/connect "ws://localhost:9003" :verbose true)
+
+(def app-state (atom {}))
+
+(defn foo [state owner]
+  (reify
+    om/IWillMount
+    (will-mount [_]
+      (go-loop []
+        (<! (async/timeout 1000))
+        (println "println")
+        (println "another println")
+        (recur)))
+    om/IRender
+    (render [_]
+      (dom/div #js {:onClick #(do (println "click!") false)}
+               "Click me"))))
+
+(om/root foo app-state {:target (js/document.getElementById "main")})
+
---
+++ src/user.clj
@@ -0,0 +1,12 @@
+(ns user
+  (:require cemerick.piggieback
+            weasel.repl.websocket))
+
+(defn repl []
+  (cemerick.piggieback/cljs-repl))
+
+(defn ws-repl []
+  (cemerick.piggieback/cljs-repl
+   :repl-env (weasel.repl.websocket/repl-env
+              :port 9003)))
+
